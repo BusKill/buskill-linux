5f9e96e44d9332d97ae59c0c6b3e7bde82bcf055
Michael Altfield <michael@michaelaltfield.net>
2020-03-19 12:32:21 -0400

    I finished the plaintext metadata overwrite in the LUKS header after luksErase is called. This has been tested on a luks container in a file only; I haven't actually tested this on a block device wipe yet.
    For testing, I've been using `bgrep` to search the LUKS container for a
    sequence of bytes (in hex) that are part of the LUKS standard. This
    works better for LUKS2 since it's variable and the metadata is stored in
    metadata with a set of mandatory JSON attributes that *must* be written
    to the LUKS header--wheras LUKS1 is just implicitly encoded and
    therefore not as easy to distinguish from random data.
    
     * https://github.com/tmbinc/bgrep
    
    Here's the space-seperated list of byte sequences that I'm using so far
    (in hex).
    
    sequences="4c554b53babe 4c554b53 6c756b73 6b6579736c6f74 737472697065 6f6666736574 68617368 61726561 656e6372797074 73616c74 7365676d656e74 6a736f6e"
    
    That's the LUKS 0xbabe magic header, LUKS, luks, keyslot, stripe,
    offset, hash, area, encrypt, salt, segment, and json.
    
    Note that I had a larger set (including aes, sha, "}, {", etc) but I
    found too many false-positives with 2-byte and 3-byte lengths.
    
    Here's the quick one-liner command I'm using with `bgrep` to validate
    the shredding of the plaintext metadata with this script so far:
    
    device=luksVol2; for s in ${sequences}; do offset=`bgrep ${s} ${device} | cut -d' ' -f2`; echo -e "${s}\n----------"; for o in ${offset}; do hexdump -Cs "0x${o}" -n16 ${device}; done; echo '=========='; done
    
    Next I'd like to actually test this self-destruct sequence on a
    disposable OS, but I'm afraid that cryptsetup, head, and all the other
    commands will be inaccessible as soon as the `luksSuspned` is called. So
    the next logical TODO is actually to setup the ramfs just before the
    `luksSuspend` with all the tools needed to finish the execution of this
    script.

--- triggers/buskill-selfdestruct.sh
+++ triggers/buskill-selfdestruct.sh
@@ -28,7 +28,9 @@ CAT=`which cat` || echo "ERROR: Unable to find cat"
 GREP=`which grep` || echo "ERROR: Unable to find grep"
 ECHO=`which echo` || echo "ERROR: Unable to find echo"
 AWK=`which awk` || echo "ERROR: Unable to find awk"
+HEAD=`which head` || echo "ERROR: Unable to find head"
 LSBLK=`which lsblk` || echo "ERROR: Unable to find lsblk"
+OD=`which od` || echo "ERROR: Unable to find od"
 
 ##############
 # ROOT CHECK #
@@ -105,24 +107,69 @@ for device in $( ${LS} -1 "/dev/mapper" ); do
 done
 
 # overwrite luks headers
-${ECHO} "INFO: shredding LUKS header (master encryption keys)"
+${ECHO} "INFO: shredding LUKS header (plaintext metadata and keyslots with encrypted master decryption key)"
 writes=''
 IFS=$'\n'
 for line in $( ${LSBLK} --list --output 'PATH,FSTYPE' | ${GREP} 'crypto_LUKS' ); do
+
 	device="`${ECHO} \"${line}\" | ${AWK} '{print \$1}'`"
 	${ECHO} -e "\t${device}"
 
-	# erases all keyslots, making the LUKS container "permanently inaccessible"
-	cryptsetup luksErase --batch-mode "${device}" || head -c 20M /dev/urandom > ${device} &
+	###########################
+	# OVERWRITE LUKS KEYSLOTS #
+	###########################
 
-	# TODO: overwrite plaintext metadata
+	# erases all keyslots, making the LUKS container "permanently inaccessible"
+	${CRYPTSETUP} luksErase --batch-mode "${device}" || ${HEAD} --bytes 20M /dev/urandom > ${device} &
 
 	# store the pid of the above write tasks so we can try to wait for it to
 	# flush to disk later -- before triggering a brutal hard-shutdown
 	writes="${writes} $!"
+
+	#####################################
+	# OVERWRITE LUKS PLAINTEXT METADATA #
+	#####################################
+
+	luksVersion=`${OD} --skip-bytes 6 --read-bytes 2 --format d2 --endian=big --address-radix "n" "${device}"`
+
+	# get the end byte to overwrite. For more info, see:
+	# https://security.stackexchange.com/questions/227359/how-to-determine-start-and-end-bytes-of-luks-header
+	if [[ $luksVersion -eq 1 ]]; then
+		# LUKS1: https://gitlab.com/cryptsetup/cryptsetup/-/wikis/LUKS-standard/on-disk-format.pdf
+
+	 	# in LUKS1, the whole header ends at 512 * the `payload-offset`
+		# this is actually more than we need (includes keyslots), but
+		# it's the fastest/easiest to bound to fetch in LUKS1
+		payloadOffset=`${OD} --skip-bytes 104 --read-bytes 4 --format d4 --endian=big --address-radix "n" "${device}"`
+		luksEndByte=$(( 512 * ${payloadOffset} ))
+
+	elif [[ $luksVersion -eq 2 ]]; then
+		# LUKS2: https://gitlab.com/cryptsetup/LUKS2-docs/blob/master/luks2_doc_wip.pdf
+
+		# in LUKS2, the end of the plaintext metadata area is twice the
+		# size of the `hdr_size` field
+		hdr_size=`${OD} --skip-bytes 8 --read-bytes 8 --format d8 --endian=big --address-radix "n" "${device}"`
+		luksEndByte=$(( 2 * ${hdr_size} ))
+
+	else
+		# version unclear; just overwrite 20 MiB
+		luksEndByte=20971520
+
+	fi
+		
+	# finally, shred that plaintext metadata; we do this in a new file descriptor
+	# to prevent bash from truncating if ${device} is a file
+	exec 5<> "${device}"
+	${HEAD} --bytes "${luksEndByte}" /dev/urandom >&5 &
+	writes="${writes} $!"
+	exec 5>&-
 done
 
-# TODO: wait until all the write tasks above have completed
+#######################
+# WAIT ON DISK WRITES #
+#######################
+
+# wait until all the write tasks above have completed
 wait "${writes}"
 
 # clear write buffer to ensure headers overwrites are actually synced to disks
